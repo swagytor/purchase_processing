cd1d234e33047118b4043b73f4d73ced083726a9
Игнатенко Алексей <swagytor@yandex.ru>
2026-04-03 00:20:31 +0300

    Add Item model

---
+++ app/models/item.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Integer, Column, String
+from sqlalchemy.orm import relationship
+
+from core.db import Base
+
+
+class Item(Base):
+    """Модель товара"""
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False, comment="Название")
+    description = Column(String, nullable=True, comment="Описание")
