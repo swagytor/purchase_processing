3b94c0609d9c87f413d75cb791714ee2bfe6f8f6
Игнатенко Алексей <swagytor@yandex.ru>
2026-04-03 04:50:07 +0300

    Add routers for Item and ItemStock

--- app/main.py
+++ app/main.py
@@ -3,8 +3,11 @@ from sqladmin import Admin
 
 import admin as admin_views
 from core.db import engine
+from routers import router
 
 app = FastAPI(title="Purchase Processing Service")
+app.include_router(router)
+
 # SQLAdmin panel
 admin = Admin(app, engine=engine)
 admin.add_view(admin_views.ItemAdmin)
